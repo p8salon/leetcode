a302e70a33735e24d29b65d370c0273418148489
p8salon <2274786223@qq.com>
2024-11-27 00:05:11 +0800

    leetcode 206 revert linklist 2method.
    Signed-off-by: p8salon <2274786223@qq.com>

--- leetcode_206_revert_ll.cpp
+++ leetcode_206_revert_ll.cpp
@@ -2,21 +2,35 @@
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
- *     struct ListNode *next;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* cur = head;
+        ListNode* post = nullptr;
+        ListNode* pre = nullptr;
 
-struct ListNode* reverseList(struct ListNode* head){
-    struct ListNode *p = NULL;
-    struct ListNode *p_r = head;
-    struct ListNode *t;
-    
-    while(p_r != NULL)
-    {
-        t = p_r->next;
-        p_r->next = p;
-        p = p_r;
-        p_r = t;
+        while(cur) {
+            post = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = post;
+        }
+
+        return cur;
     }
-    return p;
-}
+};
+
+ListNode* reverseList_res(ListNode* head) {
+        if (head == nullptr || head->next == nullptr)
+            return head;
+        ListNode *ret = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+         return ret;
+    }
